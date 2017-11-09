9d6cc4fa6939cbe91196c9a8d0166567e06f659f
ektrah <klaus.hartke@gmail.com>
2017-11-09 19:42:36 +0100

    Improve ToString

--- src/Compiler/Symbols/TypeSymbol.cs
+++ src/Compiler/Symbols/TypeSymbol.cs
@@ -15,10 +15,12 @@ namespace Mango.Compiler.Symbols
         public override int GetHashCode() => System.Runtime.CompilerServices.RuntimeHelpers.GetHashCode(this);
 
         public override string ToString() =>
-            (this is ReferenceTypeSymbol reference ? "&" + reference.ReferencedType.ToString() + "" : "") +
-            (this is ArrayTypeSymbol array ? "[" + array.ElementType.ToString() + "; " + array.Length.ToString() + "]" : "") +
-            (this is SpanTypeSymbol span ? "&[" + span.ElementType.ToString() + "]" : "") +
-            (this is NamedTypeSymbol named ? named.Name : "");
+            this is ReferenceTypeSymbol reference ? "&" + reference.ReferencedType :
+            this is ArrayTypeSymbol array ? "[" + array.ElementType + ";" + array.Length + "]" :
+            this is SpanTypeSymbol span ? "&[" + span.ElementType + "]" :
+            this is FunctionTypeSymbol func ? func.ReturnType + "(" + string.Join(", ", func.ParameterTypes) + ")" :
+            this is NamedTypeSymbol named ? named.Name :
+            Kind.ToString();
 
         internal static bool ValidBaseType(TypeSymbol type) => type == null || type.SpecialType == SpecialType.None && type.Kind == SymbolKind.NamedType;
 
